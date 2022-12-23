3aba54cbb40a98acab55bc1f413a39055fc44393
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-12-23 07:34:38 +0100

    feat: more tests

--- packages/graphql-toolbox/vite.config.ts
+++ packages/graphql-toolbox/vite.config.ts
@@ -13,22 +13,22 @@ export default defineConfig({
         port: 4141,
     },
     base: "./",
-    esbuild: {
-        target: "es2021",
-    },
+    // esbuild: {
+    //     target: "es2021",
+    // },
     // optimizeDeps: {
     //     esbuildOptions: {
     //         // mainFields: ["module", "main"],
     //         resolveExtensions: [".ts", ".tsx", ".mjs", ".json", ".js"],
     //     },
     // },
-    resolve: {
-        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
-    },
+    // resolve: {
+    //     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+    // },
     build: {
         rollupOptions: {
             plugins: [nodePolyfills({ include: null })],
         },
-        target: "es2021",
+        // target: "es2021",
     },
 });
